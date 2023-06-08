e4bae098bceffb647d3a9e4be2e499102cd3190e
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2023-06-08 16:52:34 +0530

    TABEL DATA

--- src/Components/ComponentsL3/ShowImageCart.jsx
+++ src/Components/ComponentsL3/ShowImageCart.jsx
@@ -3,36 +3,34 @@ import LoadingGif from "../../Asset/Img/Loading_Img.gif";
 import ReactImageMagnify from "react-image-magnify";
 
 const ShowImageCart = (props) => {
-  const [ImgLoad, setImgLoad] = useState(true);
   const { imageURL } = props;
+  const [ImgLoad, setImgLoad] = useState(true);
   return (
-    <>
-      <ReactImageMagnify
-        {...{
-          smallImage: {
-            src: ImgLoad ? LoadingGif : `${imageURL}.jpg`,
-            height: 233.5,
-            width: window.innerWidth * (22.61145833 / 100),
-            onLoad: () => {
-              imageURL.length <= 0 ? setImgLoad(true) : setImgLoad(false);
-            },
-            alt: "Image_Not Available",
+    <ReactImageMagnify
+      {...{
+        smallImage: {
+          src: ImgLoad ? LoadingGif : `${imageURL}.jpg`,
+          height: 233.5,
+          width: window.innerWidth * (22.61145833 / 100),
+          onLoad: () => {
+            imageURL.length <= 0 ? setImgLoad(true) : setImgLoad(false);
           },
-          largeImage: {
-            src: ImgLoad ? LoadingGif : `${imageURL}.jpg`,
-            width: 600,
-            height: 500,
-            onLoad: () => {
-              imageURL.length <= 0 ? setImgLoad(true) : setImgLoad(false);
-            },
-            alt: "Image_Not Available",
+          alt: "Image_Not Available",
+        },
+        largeImage: {
+          src: ImgLoad ? LoadingGif : `${imageURL}.jpg`,
+          width: 600,
+          height: 500,
+          onLoad: () => {
+            imageURL.length <= 0 ? setImgLoad(true) : setImgLoad(false);
           },
-          shouldUsePositiveSpaceLens: true,
-          enlargedImagePosition: "over",
-          enlargedImageClassName: "large_img",
-        }}
-      />
-    </>
+          alt: "Image_Not Available",
+        },
+        shouldUsePositiveSpaceLens: true,
+        enlargedImagePosition: "over",
+        enlargedImageClassName: "large_img",
+      }}
+    />
   );
 };
 
