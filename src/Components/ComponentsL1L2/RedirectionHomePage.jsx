ff5414f30368949c7ff8b51bfaa290008fdcedd0
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2023-06-15 16:13:13 +0530

    updated

--- src/Components/ComponentsL1L2/RedirectionHomePage.jsx
+++ src/Components/ComponentsL1L2/RedirectionHomePage.jsx
@@ -3,74 +3,46 @@ import TopHeader from "../../Common/TopHeader";
 import "../../Style/RedirectionTab.css";
 import { Link } from "react-router-dom";
 import BGImage from "../../Asset/Img/L1L2Background.jpg";
-import {
-  BsFillHouseDoorFill,
-  BsFillBarChartFill,
-  BsFillFileEarmarkPostFill,
-} from "react-icons/bs";
 
 const RedirectionHomePage = () => {
   const ROLE = localStorage.getItem("indent-expressRole");
-  console.log("ROLE==>", ROLE);
   return (
     <>
       <TopHeader />
       <div className="DropDownFormStyle">
         <div className="row w-100">
           {ROLE === "L1" || ROLE === "L2" ? (
-            <div className="d-flex">
-              <Link to="/Indent-express/direction/home">
-                <BsFillHouseDoorFill size={25} className="my-2 text-dark" />
+            <div className="d-flex w-100 mx-3">
+              <Link
+                className="col-md-6 redirectionTab"
+                to="/Indent-express/L1/L2/physical/home"
+              >
+                PHYSICAL
               </Link>
-              <Link to="/Indent-express/L1/L2/products/reports">
-                <BsFillFileEarmarkPostFill
-                  size={25}
-                  className="my-2 text-dark mx-3"
-                />
+              <Link
+                className="col-md-6 redirectionTab"
+                to="/Indent-express/feedback/L1/L2"
+              >
+                DIGITAL
               </Link>
-              <Link to="/Indent-express/L1/L2/status/reports">
-                <BsFillBarChartFill size={25} className="my-2 text-dark" />
-              </Link>
-              <div className="d-flex w-100 mx-3">
-                <Link
-                  className="col-md-6 redirectionTab"
-                  to="/Indent-express/L1/L2/physical/home"
-                >
-                  PHYSICAL
-                </Link>
-                <Link
-                  className="col-md-6 redirectionTab"
-                  to="/Indent-express/feedback/L1/L2"
-                >
-                  DIGITAL
-                </Link>
-              </div>
             </div>
           ) : (
             ""
           )}
           {ROLE === "L3" && (
-            <div className="d-flex">
-              <Link to="/Indent-express/direction/home">
-                <BsFillHouseDoorFill size={25} className="mt-2 text-dark" />
+            <div className="d-flex w-100 mx-3">
+              <Link
+                className="col-md-6 redirectionTab"
+                to="/Indent-express/L3/physical/home"
+              >
+                PHYSICAL
               </Link>
-              <Link to="/Indent-express/L3/status/reports">
-                <BsFillBarChartFill size={25} className="mt-2 mx-2 text-dark" />
+              <Link
+                className="col-md-6 redirectionTab"
+                to="/Indent-express/L3/home"
+              >
+                DIGITAL
               </Link>
-              <div className="d-flex w-100 mx-3">
-                <Link
-                  className="col-md-6 redirectionTab"
-                  to="/Indent-express/L3/physical/home"
-                >
-                  PHYSICAL
-                </Link>
-                <Link
-                  className="col-md-6 redirectionTab"
-                  to="/Indent-express/L3/home"
-                >
-                  DIGITAL
-                </Link>
-              </div>
             </div>
           )}
         </div>
