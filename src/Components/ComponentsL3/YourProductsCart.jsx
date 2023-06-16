fb008145d0a9e9a9ebbed501c9be592557d1e3f5
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2023-06-16 12:38:04 +0530

    updated

--- src/Components/ComponentsL3/YourProductsCart.jsx
+++ src/Components/ComponentsL3/YourProductsCart.jsx
@@ -11,6 +11,7 @@ import * as Icon from "react-bootstrap-icons";
 import ReactHTMLTableToExcel from "react-html-table-to-excel";
 import { ItemWiseReportsDropdown, L1L2HeadingData } from "../../Data/DataList";
 import TablePagination from "@mui/material/TablePagination";
+import Loader from "../../Common/Loader";
 
 const YourProductsCart = () => {
   const [cartProducts, setCartProducts] = useState([]);
@@ -18,14 +19,17 @@ const YourProductsCart = () => {
   const [rowsPerPage, setRowsPerPage] = useState(50);
   const [itemWiseValue, setItemWiseValue] = useState("");
   const [searchItemCode, setSearchItemCode] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get("https://jsonplaceholder.typicode.com/users")
       .then((res) => res)
       .then((response) => {
         setCartProducts(response.data);
       });
+    setLoading(false);
   }, [itemWiseValue]);
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -37,6 +41,7 @@ const YourProductsCart = () => {
   };
   return (
     <>
+      {loading === true ? <Loader /> : ""}
       <TopHeader />
       <div className="ComponentL3LowerHeader">
         <div className="d-flex mx-2 w-100">
@@ -65,94 +70,105 @@ const YourProductsCart = () => {
           <BsCartFill size={25} className="mt-2 mx-2 text-dark" />
         </Link>
       </div>
-      <br />
-      <div className="row d-flex justify-content-between mx-0 my-2">
-        <div className="col-md-3">
-          <input
-            type="text"
-            value={searchItemCode}
-            className="SearchRowByItem"
-            placeholder="Search by Item Code"
-            onChange={(e) => setSearchItemCode(e.target.value)}
-          />
-        </div>
-        <div className="col-md-4 confirmButtons">
-          <button className="confirmSendmail mx-2">CONFIRM</button>
-          <button className="confirmSendmail">SEND MAIL</button>
-        </div>
-      </div>
       {cartProducts.length > 0 && (
-        <div className="table-responsive mx-1">
-          <table className="table table-hover table-bordered" id="table-to-xls">
-            <thead>
-              <tr>
-                {L1L2HeadingData.map((item, i) => {
-                  return (
-                    <th key={i} className="tableHeading">
-                      {item.label}
-                    </th>
-                  );
-                })}
-              </tr>
-            </thead>
-            <tbody>
-              {cartProducts
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((item, i) => {
-                  const { itemCode } = item;
-                  const imageCode = !itemCode ? "" : itemCode.substring(2, 9);
-                  const imageURL = `https://jewbridge.titanjew.in/CatalogImages/api/ImageFetch/?Type=ProductImages&ImageName=${imageCode}.jpg`;
-                  return (
-                    <tr key={i} className="tableRowData">
-                      <td>{item.id}</td>
-                      <td className="text-center">
-                        <img
-                          src={imageURL}
-                          width="70"
-                          height="70"
-                          className="img-thumbnail"
-                          alt="No_Image"
-                        />
-                      </td>
-                      <td>{item.itemCode}</td>
-                      <td>{item.collection}</td>
-                      <td>{item.consumerBase}</td>
-                      <td>{item.itGroup}</td>
-                      <td>{item.category}</td>
-                      <td>{item.stdWt}</td>
-                      <td>{item.stdUCP}</td>
-                      <td>{item.saleable}</td>
-                      <td>{item.reasons}</td>
-                      <td className="text-center">
-                        <Icon.PencilSquare
-                          size={18}
-                          cursor="pointer"
-                          // onClick={() => EditReport(item)}
-                        />
-                      </td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
-          <div className="d-flex justify-content-end my-2 w-100">
-            <ReactHTMLTableToExcel
-              id="test-table-xls-button"
-              className="excelButton"
-              table="table-to-xls"
-              filename={itemWiseValue}
-              sheet="tablexls"
-              buttonText="DOWNLOAD"
-            />
-            <TablePagination
-              rowsPerPageOptions={[50, 100, 150, cartProducts.length]}
-              component="div"
-              count={cartProducts.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-            />
+        <div>
+          <div className="row d-flex justify-content-between mx-0 my-3">
+            <div className="col-md-3">
+              <input
+                type="text"
+                value={searchItemCode}
+                className="SearchRowByItem"
+                placeholder="Search by Item Code"
+                onChange={(e) => setSearchItemCode(e.target.value)}
+              />
+            </div>
+            <div className="col-md-4 confirmButtons">
+              <button className="confirmSendmail mx-2">CONFIRM</button>
+              <button className="confirmSendmail">SEND MAIL</button>
+            </div>
+          </div>
+          <div className="table-responsive mx-1">
+            <table
+              className="table table-hover table-bordered"
+              id="table-to-xls"
+            >
+              <thead>
+                <tr>
+                  {L1L2HeadingData.map((item, i) => {
+                    return (
+                      <th key={i} className="tableHeading">
+                        {item.label}
+                      </th>
+                    );
+                  })}
+                </tr>
+              </thead>
+              <tbody>
+                {cartProducts
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((item, i) => {
+                    const { itemCode } = item;
+                    const imageCode = !itemCode ? "" : itemCode.substring(2, 9);
+                    const imageURL = `https://jewbridge.titanjew.in/CatalogImages/api/ImageFetch/?Type=ProductImages&ImageName=${imageCode}.jpg`;
+                    return (
+                      <tr key={i} className="tableRowData">
+                        <td>{item.id}</td>
+                        <td className="text-center">
+                          <img
+                            src={imageURL}
+                            width="70"
+                            height="70"
+                            className="img-thumbnail"
+                            alt="No_Image"
+                          />
+                        </td>
+                        <td>{item.itemCode}</td>
+                        <td>{item.collection}</td>
+                        <td>{item.consumerBase}</td>
+                        <td>{item.itGroup}</td>
+                        <td>{item.category}</td>
+                        <td>{item.stdWt}</td>
+                        <td>{item.stdUCP}</td>
+                        <td>{item.saleable}</td>
+                        <td>{item.reasons}</td>
+                        <td className="text-center">
+                          <Icon.PencilSquare
+                            size={18}
+                            cursor="pointer"
+                            className="mx-2"
+                            // onClick={() => EditReport(item)}
+                          />
+                          <Icon.Trash
+                            size={18}
+                            cursor="pointer"
+                            className="text-danger"
+                            // onClick={() => EditReport(item)}
+                          />
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+            <div className="d-flex justify-content-end my-2 w-100">
+              <ReactHTMLTableToExcel
+                id="test-table-xls-button"
+                className="excelButton"
+                table="table-to-xls"
+                filename={itemWiseValue}
+                sheet="tablexls"
+                buttonText="DOWNLOAD"
+              />
+              <TablePagination
+                rowsPerPageOptions={[50, 100, 150, cartProducts.length]}
+                component="div"
+                count={cartProducts.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </div>
           </div>
         </div>
       )}
