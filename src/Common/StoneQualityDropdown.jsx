48dd2b41db8715509cc2739278b10c269de14a62
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2023-06-20 13:02:25 +0530

    updated

--- src/Common/StoneQualityDropdown.jsx
+++ src/Common/StoneQualityDropdown.jsx
@@ -2,13 +2,37 @@ import React from "react";
 import "../Style/ShowImage.css";
 
 const StoneQualityDropdown = (props) => {
-  const { optionsList, GetStoneData, singleProductsDetails } = props;
-  const stdUCP = singleProductsDetails.stdUCP;
+  const { GetStoneData, singleProductsDetails } = props;
+
+  const stoneOptions = (inputObj) => {
+    let stoneOptionList = [];
+    if (inputObj.stdUCP) {
+      stoneOptionList[1 + stoneOptionList.length] = `stdUCP-${inputObj.stdUCP}`;
+    }
+    if (inputObj.si2Gh) {
+      stoneOptionList[1 + stoneOptionList.length] = `si2Gh-${inputObj.si2Gh}`;
+    }
+    if (inputObj.vsGh) {
+      stoneOptionList[1 + stoneOptionList.length] = `vsGh-${inputObj.vsGh}`;
+    }
+    if (inputObj.vvs1) {
+      stoneOptionList[1 + stoneOptionList.length] = `vvs1-${inputObj.vvs1}`;
+    }
+    if (inputObj.i2Gh) {
+      stoneOptionList[1 + stoneOptionList.length] = `i2Gh-${inputObj.i2Gh}`;
+    }
+    if (inputObj.si2Ij) {
+      stoneOptionList[1 + stoneOptionList.length] = `si2Ij-${inputObj.si2Ij}`;
+    }
+    return stoneOptionList;
+  };
+
+  const optionsList = stoneOptions(singleProductsDetails);
+
   return (
     <div className="mt-3">
       <select onChange={GetStoneData} className="L3SelectDropdown">
         <option value="">Select Stone Quality</option>
-        <option value="stdUCP-289237">stdUCP-{stdUCP}</option>
         {optionsList.map((item, i) => {
           return <option key={i}>{item}</option>;
         })}
