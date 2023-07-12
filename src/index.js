7449c1cf56a6c28744996ae1115700104c722321
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2023-07-12 09:54:30 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Helmet } from "react-helmet";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Helmet
+      contentSecurityPolicy="
+        default-src 'self';
+        script-src 'self' 'unsafe-inline';
+        style-src 'self' 'unsafe-inline';
+        img-src 'self' data:;"
+    />
+    <Helmet permissionsHeader="geolocation=(), microphone=()" />
+    <Helmet referrerPolicy="policy 'strict-origin'" />
+    <Helmet xContentTypeOption="X-Content-Type-Options 'nosniff'" />
+    <Helmet xFrameOption="X-Frame-Options 'sameorigin'" />
+    <Helmet strictTransportSecurity="Strict-Transport-Security 'max-age=31536000'" />
     <App />
   </React.StrictMode>
 );
