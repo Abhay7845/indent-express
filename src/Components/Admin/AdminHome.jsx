3ad1132f4e1805ce1c6f55dc514fe476ecd74728
abhay kumar <abhaykr95678@gmail.com>
2023-07-19 16:31:50 +0530

    updated

--- src/Components/Admin/AdminHome.jsx
+++ src/Components/Admin/AdminHome.jsx
@@ -5,7 +5,6 @@ import TopHeader from "../../Common/TopHeader";
 import AdminSideBar from "./AdminSideBar";
 import "../../Style/AdminHome.css";
 import AdiminFileSideBar from "./AdiminFileSideBar";
-import { DatePicker, Input } from "antd";
 
 const AdminHome = () => {
   return (
@@ -17,15 +16,19 @@ const AdminHome = () => {
       <AdiminFileSideBar />
       <div className='main'>
         <h5 className='text-center mt-2'>COPY STORE INDENTS</h5>
-        <div className='row g-2 mt-2 mx-1'>
+        <div className='row g-3 mt-2 mx-1'>
           <div className='col-md-4'>
-            <DatePicker className='DateSelect' />
+            <input
+              type='date'
+              className='DateSelect'
+              placeholder='select date'
+            />
           </div>
           <div className='col-md-4'>
-            <Input className='AInpute' placeholder='From Store Code' />
+            <input className='AInpute' placeholder='From Store Code' />
           </div>
           <div className='col-md-4'>
-            <Input className='AInpute' placeholder='To Store Code' />
+            <input className='AInpute' placeholder='To Store Code' />
           </div>
         </div>
         <div className='d-flex justify-content-end mt-3 mx-2'>
--- src/Components/Admin/UpdatePortalStatus.jsx
+++ src/Components/Admin/UpdatePortalStatus.jsx
@@ -4,7 +4,6 @@ import React from "react";
 import TopHeader from "../../Common/TopHeader";
 import AdminSideBar from "./AdminSideBar";
 import AdiminFileSideBar from "./AdiminFileSideBar";
-import { DatePicker, Input } from "antd";
 
 const UpdatePortalStatus = () => {
   return (
@@ -18,15 +17,19 @@ const UpdatePortalStatus = () => {
       <AdiminFileSideBar />
       <div className='main'>
         <h5 className='text-center mt-2'>UPDATE PORTAL STATUS</h5>
-        <div className='row g-2 mt-2 mx-1'>
+        <div className='row g-3 mt-2 mx-1'>
           <div className='col-md-4'>
-            <DatePicker className='DateSelect' />
+            <input
+              type='date'
+              className='DateSelect'
+              placeholder='select date'
+            />
           </div>
           <div className='col-md-4'>
-            <Input className='AInpute' placeholder='From Store Code' />
+            <input className='AInpute' placeholder='From Store Code' />
           </div>
           <div className='col-md-4'>
-            <Input className='AInpute' placeholder='To Store Code' />
+            <input className='AInpute' placeholder='To Store Code' />
           </div>
         </div>
         <div className='d-flex justify-content-end mt-3 mx-2'>
